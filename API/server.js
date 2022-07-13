ff10caf73f6af11e17e17627f6ca1bb5a45a07cd
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-07-14 00:34:41 +0200

    feat: fetching random data inside server.js using axios package

--- API/server.js
+++ API/server.js
@@ -2,12 +2,23 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const PORT =  8000
+const axios = require('axios')
 
 app.use(cors())
+
 app.use(express.static('public'));
+
 app.get('/', (req, res) =>{
   res.status(200).sendFile(__dirname + '/public/index.html')
 })
+
+axios.get('http://api.mediastack.com/v1/news?access_key=faf51760005ac738971d6003d73e8cfd&countries=nz')
+  .then(response => {
+        const apiResponse = response.data;
+        console.log(apiResponse);
+  }).catch(error => {
+        console.log(error);
+  });
 
 app.get('/post/:slug', (req, res) => {
   res.sendFile(path.join(__dirname, '../post.html'))
