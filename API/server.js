3cd63fad6edd764dbcfb177f74ba6f82c60aecd4
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-07-14 21:07:16 +0200

    feat: fetch the article by category and title

--- API/server.js
+++ API/server.js
@@ -3,40 +3,47 @@ const app = express()
 const cors = require('cors')
 const PORT =  8000
 const axios = require('axios')
+const bodyParser = require('body-parser')
 const sanityClient = require("./client")
-
+app.use(bodyParser.json());
 app.use(cors())
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>{
-  res.status(200).sendFile(__dirname + '/public/index.html')
-})
 
-axios.get('http://api.mediastack.com/v1/news?access_key=faf51760005ac738971d6003d73e8cfd&countries=nz')
-  .then(response => {
-        const randomArticles = response.data.data;
-        const sanityPosts = randomArticles.map(article =>({
-          _type: 'post',
-          title: article.title,
-          slug: article.url,
-          author: article.author,
-          categories: article.category,
-          mainImage: article.image,
-          publishedAt: article.published_at,
-          body: article.description
-          }
-        ))
-        for (post of sanityPosts){
-          sanityClient.create(post)
-        }
-        console.log(sanityPosts);
-  }).catch(error => {
-        console.log(error);
-  });
+// axios.get('http://api.mediastack.com/v1/news?access_key=faf51760005ac738971d6003d73e8cfd&countries=nz')
+//   .then(response => {
+//         const randomArticles = response.data.data;
+//         const sanityPosts = randomArticles.map(article =>({
+//           _type: 'post',
+//           title: article.title,
+//           author: article.author,
+//           category: article.category,
+//           mainImage: article.image,
+//           publishedAt: article.published_at,
+//           body: article.description
+//           }
+//         ))
+//         for (const post of sanityPosts){
+//           sanityClient.create(post)
+//         }
+//         console.log(sanityPosts.length);
+//   }).catch(error => {
+//         console.log(error);
+//   });
 
-app.get('/post/:slug', (req, res) => {
-  res.sendFile(path.join(__dirname, '../post.html'))
+app.get('/:category/:article', (req, res) => {
+  const categoryParam = req.params.category
+  const articleTitleParam = req.params.article
+  
+  sanityClient.fetch(
+    `*[_type == "post" && category == $categoryParam && title == $articleTitleParam]`,
+    { categoryParam: categoryParam,
+      articleTitleParam: articleTitleParam}
+   
+    ).then((data => res.status(200).send(data)))
+  // res.status(200).send(post)
+//  console.log(post)
 })
 
 app.listen(process.env.PORT || PORT, ()=>{
