67f5e8144ade921b48c103e233346fac4569aa3e
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-07-15 05:53:18 +0200

    feat removed autor, category and body from post component

--- blog_front/src/components/post.js
+++ blog_front/src/components/post.js
@@ -1,13 +1,18 @@
 
+import { Link } from "react-router-dom";
+
 export default function Post({title, author, category, mainImage, body}){
+
+
   return(
-    <article>
-      <h2>{title}</h2>
-      <h3>{author}</h3>
-      <h3>{category}</h3>
+    <div className="post">
+      <img className="mainImage" src={mainImage} alt="" />
+      <Link to={`/${category}/${title}`}><h4>{title}</h4></Link>
+      {/* <h3>{author}</h3> */}
+      {/* <h3>{category}</h3> */}
       
-      <img src={mainImage} alt="" />
-      <p>{body}</p>
-    </article>
+      
+      {/* <p>{body}</p> */}
+    </div>
   )
 }
