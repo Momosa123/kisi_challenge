590d8afcca9833b43e95441141f38e993d0b813a
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-07-16 23:45:47 +0200

    update: change the name of the blog

--- blog_front/src/components/Header.js
+++ blog_front/src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header(){
   return(
     <div>
       <header>
-        <h1 type="button" >BNN</h1>
+        <h1 type="button" >KISI Blog</h1>
         <Search/>
       </header>
     </div>
