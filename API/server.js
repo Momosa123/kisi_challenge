d227ac32eb11787645b645085bf0bed4cc2ad755
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-07-16 21:12:50 +0200

    feat: decode special entities

--- API/server.js
+++ API/server.js
@@ -5,7 +5,8 @@ const PORT =  4000
 const axios = require('axios')
 const bodyParser = require('body-parser')
 const sanityClient = require("./client")
-
+//html-entities to convert special characters
+const entities = require('html-entities')
 app.use(bodyParser.json());
 app.use(cors())
 //to be removed
@@ -13,35 +14,36 @@ sanityClient.delete({
   query: `*[_type == "post"]`
 })
 const path = require('path');
-//Get all the datta from the mediastack API and put them  to the sanity studio
+
 //creating dummy categories as the fetch datas have none
 const categories =['innovation', 'funding', 'FAANG', 'tech']
 let randomIndex = Math.floor(Math.random()*5)
-console.log(randomIndex)
+
+//Get all the datta from the mediastack API and put them  to the sanity studio
 //we fetched the data once so we comment the code for not fetching it again
 const url = 'https://techcrunch.com/'
 const endpoint = 'wp-json/wp/v2/posts?per_page=25&context=view'
 
-axios.get(url + endpoint)
-  .then(response => {
-        const randomArticles = response.data;
-        const sanityPosts = randomArticles.map(article =>({
-          _type: 'post',
-          title: article.title.rendered,
-          author: article.parsely.meta.creator[0],
-          category: categories[randomIndex],
-          mainImage: article.jetpack_featured_media_url,
-          publishedAt: article.parsely.meta.datePublished,
-          body: article.content.rendered
-          }
-        ))
-        for (const post of sanityPosts){
-          sanityClient.create(post)
-        }
-        console.log(randomArticles[0]);
-  }).catch(error => {
-        console.log(error);
-  });
+// axios.get(url + endpoint)
+//   .then(response => {
+//         const randomArticles = response.data;
+//         const sanityPosts = randomArticles.map(article =>({
+//           _type: 'post',
+//           title: entities.decode(article.title.rendered),
+//           author: article.parsely.meta.creator[0],
+//           category: categories[randomIndex],
+//           mainImage: article.jetpack_featured_media_url,
+//           publishedAt: article.parsely.meta.datePublished,
+//           body: article.content.rendered
+//           }
+//         ))
+//         for (const post of sanityPosts){
+//           sanityClient.create(post)
+//         }
+//         console.log(randomArticles[0]);
+//   }).catch(error => {
+//         console.log(error);
+//   });
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../blog_front/build')));
