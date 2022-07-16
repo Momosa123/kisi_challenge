039050779ce65bddd076a8661ce95abed18eff9f
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-07-16 23:47:31 +0200

    changed the folder

---
+++ blog_front/src/pages/AllPosts.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+import Post from "../components/PostPreview"
+
+export default function AllPosts(){
+ const [posts, setPosts] = useState([])
+ const [loading, setLoading] = useState(true);
+ useEffect(
+   ()=>{
+     async function getPosts() {
+  
+       const res = await fetch("/api")
+       const data = await res.json()
+       console.log(data)
+       setPosts(data.filter(post => post.mainImage != null))
+       setLoading(false);
+   }
+   getPosts()
+   },[]
+ )
+ const postElements = posts.map(
+   (post, i) => <Post
+   key={i} 
+   category = {post.category} 
+   title = {post.title} 
+   mainImage ={post.mainImage} 
+   author={post.author} 
+   body={post.body}  
+   />
+   )
+ return (
+   <div className="grid">
+   { loading ? <h3>Loading...</h3> :
+    postElements}
+   </div>
+ );
+}
