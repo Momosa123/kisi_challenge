11431e4aadf874f1c71c77c1ef562cc03985babe
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-07-17 15:20:33 +0200

    feat: navigate to home on header title click

--- blog_front/src/components/Header.js
+++ blog_front/src/components/Header.js
@@ -1,12 +1,16 @@
+import {useNavigate} from "react-router-dom"
 import Search from "./Search";
 
 
 export default function Header(){
-
+  let navigate = useNavigate()
+function handleClick(){
+  navigate("/")
+}
   return(
     <div>
       <header>
-        <h1 type="button" >KISI Blog</h1>
+        <h1 onClick={handleClick} className="header-title" type="button" >KISI Blog</h1>
         <Search/>
       </header>
     </div>
