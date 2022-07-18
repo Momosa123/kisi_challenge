8386e4a0a634cfbea4744f76d7b61599b8e27ad9
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-07-18 02:51:51 +0200

    small changes on title

--- blog_front/src/components/Header.js
+++ blog_front/src/components/Header.js
@@ -4,13 +4,17 @@ import Search from "./Search";
 
 export default function Header(){
   let navigate = useNavigate()
-function handleClick(){
-  navigate("/")
-}
+  function handleClick(){
+    navigate("/")
+    
+  }
+
+ 
   return(
     <div>
       <header>
         <h1 onClick={handleClick} className="header-title" type="button" >KISI Blog</h1>
+    
         <Search/>
       </header>
     </div>
