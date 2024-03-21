304f2254abf5020b7ef66b926ac5c9d189768ff7
Mouhamadou <mouhamadoul.sarr@gmail.com>
2024-03-21 06:55:53 +0100

    commentaire de certaines parties inutiles du server

--- API/server.js
+++ API/server.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const router = express.Router();
-const PORT =  4000
-const axios = require('axios')
-const nodemailer = require('nodemailer')
+const PORT = 4000;
+const axios = require("axios");
+const nodemailer = require("nodemailer");
 
 app.use(express.json());
-const sanityClient = require("./client")
+const sanityClient = require("./client");
 //html-entities to convert special characters
 // const entities = require('html-entities')
 // app.use(bodyParser.json());
-app.use(cors())
-app.use("/", router)
+app.use(cors());
+app.use("/", router);
 //used to delete all the documents in sanity studio if needed
 // sanityClient.delete({
 //   query: `*[_type == "post"]`
@@ -20,16 +20,15 @@ app.use("/", router)
 // sanityClient.delete({
 //   query: `*[_type == "seo"]`
 // })
-const path = require('path');
+const path = require("path");
 
 //creating dummy categories as the fetch datas have none
-const categories =['innovation', 'funding', 'FAANG', 'tech']
-
+const categories = ["innovation", "funding", "FAANG", "tech"];
 
 //Get all the data from the mediastack API and put them  to the sanity studio
 //we fetched the data once so we comment the code for not fetching it again
-const techCrunchUrl = 'https://techcrunch.com/'
-const endpoint = 'wp-json/wp/v2/posts?per_page=25&context=view'
+const techCrunchUrl = "https://techcrunch.com/";
+const endpoint = "wp-json/wp/v2/posts?per_page=25&context=view";
 
 // axios.get(techCrunchUrl + endpoint)
 //   .then(response => {
@@ -67,117 +66,102 @@ const endpoint = 'wp-json/wp/v2/posts?per_page=25&context=view'
 //   });
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../blog_front/build')));
+// app.use(express.static(path.resolve(__dirname, "../blog_front/build")));
 
 //fetch all posts from sanity studio and send them to frontend
-app.get('/api', (req, res) => {
-  
-  sanityClient.fetch(
-    `*[_type == "post"]`
-   
-    ).then((data => res.status(200).send(data)))
-})
+app.get("/api", (req, res) => {
+  sanityClient
+    .fetch(`*[_type == "post"]`)
+    .then(data => res.status(200).send(data));
+});
 
 //fetch  post based on category and title from sanity studio and send it to frontend
-app.get('/:category/:article', (req, res) => {
-  const categoryParam = req.params.category
-  const articleTitleParam = req.params.article
-  
-  sanityClient.fetch(
-    `*[_type == "post" && category == $categoryParam && title == $articleTitleParam]`,
-    { categoryParam: categoryParam,
-      articleTitleParam: articleTitleParam}
-   
-    ).then((data => {
-      console.log(data)
-        data ?
-        res.status(200).send(data) : 
-        res.json().then((body) => {
-          console.log("error fetching category and article")
-          throw new Error(body.error)
-        })
-      }
+app.get("/:category/:article", (req, res) => {
+  const categoryParam = req.params.category;
+  const articleTitleParam = req.params.article;
+
+  sanityClient
+    .fetch(
+      `*[_type == "post" && category == $categoryParam && title == $articleTitleParam]`,
+      { categoryParam: categoryParam, articleTitleParam: articleTitleParam }
     )
-  )
+    .then(data => {
+      console.log(data);
+      data
+        ? res.status(200).send(data)
+        : res.json().then(body => {
+            console.log("error fetching category and article");
+            throw new Error(body.error);
+          });
+    });
+});
 
-})
+app.get("/:article", (req, res) => {
+  const articleTitleParam = req.params.article;
 
-app.get('/:article', (req, res) => {
-
-const articleTitleParam = req.params.article
-
-  sanityClient.fetch(
-    `*[_type == "post" && title == $articleTitleParam]`,
-    { articleTitleParam: articleTitleParam}
-    
-    ).then(data => {
-      
-        if (data.length == 0) 
-        {
-        return  res.status(404).send('unknown article')
-          }
-        else {
-          
-          return res.status(200).send(data)
-        }        
-      
-      })
-
-  
-  }        
-
-)
+  sanityClient
+    .fetch(`*[_type == "post" && title == $articleTitleParam]`, {
+      articleTitleParam: articleTitleParam,
+    })
+    .then(data => {
+      if (data.length == 0) {
+        return res.status(404).send("unknown article");
+      } else {
+        return res.status(200).send(data);
+      }
+    });
+});
 
 //create transport linked to my email with nodemailer
-const contactEmail = nodemailer.createTransport({
-    service: 'gmail',
-    port: 465,
-    auth: {
-    user: "mouhamadoul.sarr@gmail.com",
-    pass: "ddxfhtiamkwkmoyk",
-  },
-  secure:true,
-});
+// const contactEmail = nodemailer.createTransport({
+//     service: 'gmail',
+//     port: 465,
+//     auth: {
+//     user: "mouhamadoul.sarr@gmail.com",
+//     pass: "ddxfhtiamkwkmoyk",
+//   },
+//   secure:true,
+// });
 
-contactEmail.verify((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Ready to Send");
-  }
-});  
+// contactEmail.verify((error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Ready to Send");
+//   }
+// });
 
 //send email on form submission
-router.post("/contact", (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const message = req.body.message; 
-  
-  const mail = {
-    from: "mouhamadoul.sarr@gmail.com",
-    to: email,
-    subject: " KISI Form Submission",
-    html: `Hello ${name},<br><br>
-    Thanks we received your extraordinary message.<br>
-            We will get bak to you as soon as possible 🚀🚀🚀 <br><br>
-            <h5>From Mouhamadou, Javascript Operations Engineer  at KISI ❤️</h5>
-            `,
+// router.post("/contact", (req, res) => {
+//   const name = req.body.name;
+//   const email = req.body.email;
+//   const message = req.body.message;
 
-  };
-  contactEmail.sendMail(mail, (error,info) => {
-    if (error) {
-      res.json({ status: "ERROR" });
-    } else {
-      res.status(200).send({message:"Mail sent", message_id: info.messageId})
-    }
-  });
-});
+//   const mail = {
+//     from: "mouhamadoul.sarr@gmail.com",
+//     to: email,
+//     subject: " KISI Form Submission",
+//     html: `Hello ${name},<br><br>
+//     Thanks we received your extraordinary message.<br>
+//             We will get bak to you as soon as possible 🚀🚀🚀 <br><br>
+//             <h5>From Mouhamadou, Javascript Operations Engineer  at KISI ❤️</h5>
+//             `,
+
+//   };
+//   contactEmail.sendMail(mail, (error,info) => {
+//     if (error) {
+//       res.json({ status: "ERROR" });
+//     } else {
+//       res.status(200).send({message:"Mail sent", message_id: info.messageId})
+//     }
+//   });
+// });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../blog_front/build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../blog_front/build", "index.html"));
 });
 
-app.listen(process.env.PORT || PORT, ()=>{
-  console.log(`Listening on port ${PORT}` );
-})
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
